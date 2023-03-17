60e2386e3fcdcfed86210f0d9e36f9b5762a61db
gipsydrawing <oscardanieljimenez97@gmail.com>
2023-03-17 13:35:40 +0100

    subbida eje1

---
+++ ejercicio1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    $variablesinvalor;
+    $booleano1 = true;
+    $booleano2 = false;
+    constant PI = 3.141592653589793;
+    $tau = PI * 2;
+    constant $minombre = Daniel;
+    constant $minumfav = 06;
+    $valor1 = $booleano1 - $booleano2;
+    $valor2 = $booleano1;
+    $valor3 = $booleano1 + $booleano2;
+    $valor4 = $booleano1 - $booleano2;
+    $suma = $booleano1 + $tau ;
+    $valor5 = $suma;
+    $booleano6 = null;
+    $valor6 = > < 9;
+    $variableNumerica = +;
+    $valor7 = $variableNumerica < (const minumfav * $tau);
+    $dos = 2;
+    $suma2 = $dos+ 5;
+    $resta = $minumfav -  $dos;
+    $minumfavmenosdos = $resta;
+    $suma3 = $minumfav + 4$$dos;
+    $multiplicacion = $minumfav + $dos;
+    $division = ($minumfav . $dos [=3]);
+    $division2 = 143 : 140;
+    $division3 = 25 : 5;
+</body>
+</html>
