aae277d92a236748e8e646b3f62cf9e5c90f284f
gipsydrawing <oscardanieljimenez97@gmail.com>
2023-03-17 13:37:51 +0100

    subbida eje4

---
+++ ejercicio4.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    $variablesinvalor;
+    $booleano1 = true;
+    $booleano2 = false;
+    constant PI = 3.141592653589793;
+    $tau = PI * 2;
+    constant minombre = Daniel
+    constant minumfav = 06
+    $comparacion = $booleano1 - $booleano2
+    
+    // Crea una variable cuyo valor sea la comparación booleana de no boleano1. //
+    $valor1 = 
+    $valor2 = $booleano1 + $booleano2
+
+</body>
+</html>
