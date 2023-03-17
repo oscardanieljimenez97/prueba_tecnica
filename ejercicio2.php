c4cd2f134956b86ae5b6feb2c6d6a4a3e6d7c263
gipsydrawing <oscardanieljimenez97@gmail.com>
2023-03-17 14:02:25 +0100

    subbida eje2

---
+++ ejercicio2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    $arrayVacio;
+    $array1 = ["1, 2, 3, 4, 5, 6, 7, 8, 9"];
+    $arrayNumeros = $array1;
+    $array2 = ["2, 4, 6, 8"];
+    $arrayNumerosPares = $array2;
+    $array3 = ["0, 1, 2"];
+    $array4 = ["a, b, c"];
+    $arrayBidimensional = $array3,$array4;
+
+
+</body>
+</html>
